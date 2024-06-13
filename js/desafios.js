26aefe845f895e72cbc555d9c2afc615a60a636d
Gabriel Azevedo <gbldja@gmail.com>
2024-06-13 14:10:40 -0300

    insere objeto desafio2

--- js/desafios.js
+++ js/desafios.js
@@ -1,7 +1,7 @@
 ﻿const desafios = [
   {
-    imgSrc: "./images/print_codelab.png",
-    imgAlt: "Print do site que fora desenvolvido",
+    imgSrc: "./images/desafio01.png",
+    imgAlt: "Print do desafio 01 Blog CodeLab",
     title: "Blog CodeLab",
     numero: "001",
     links: [
@@ -17,6 +17,28 @@
       },
       {
         href: "./desafios/01/index.html",
+        iconName: "link-outline",
+        text: "Link"
+      }
+    ]
+  },  {
+    imgSrc: "./images/desafio02.png",
+    imgAlt: "Print do desafio 02 Loja JordanShoes",
+    title: "Loja JordanShoes",
+    numero: "002",
+    links: [
+      {
+        href: "https://www.figma.com/design/Yb9IBH56g7T1hdIyZ3BMNO/Desafios---CodeLab?node-id=0-1&t=CbvMxfU5H00kVbPY-1",
+        iconName: "logo-figma",
+        text: "Figma"
+      },
+      {
+        href: "https://github.com/gabazevdo/codelab/tree/main/desafios/02",
+        iconName: "logo-github",
+        text: "Github"
+      },
+      {
+        href: "./desafios/02/index.html",
         iconName: "link-outline",
         text: "Link"
       }
